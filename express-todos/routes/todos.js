392827f8fab83b996695cc69db27e3b004830cdd
wsjoshua <ws.joshua@gmail.com>
2023-08-01 09:51:04 -0600

    update template

--- express-todos/routes/todos.js
+++ express-todos/routes/todos.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET todos index listing. - http://localhost:3000/todos/ */ 
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* GET todos show listing. - http://localhost:3000/todos/:id */ 
 
 module.exports = router;
